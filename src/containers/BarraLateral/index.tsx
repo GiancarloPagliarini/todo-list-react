638d5d3dad42ae5ae70f3d119ddb5f148843693d
Giancarlo Pagliarini Alves <giancarlopalvess@outlook.com>
2023-08-02 12:02:12 -0300

    adicao dos enums

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -8,12 +8,12 @@ const BarraLateral = () => (
       <S.Campo type="text" placeholder="buscar" />
     </div>
     <S.Filtros>
-      <FiltroCards />
-      <FiltroCards />
-      <FiltroCards />
-      <FiltroCards />
-      <FiltroCards />
-      <FiltroCards ativo />
+      <FiltroCards legenda="pendentes" contador={3} />
+      <FiltroCards legenda="concluídas" contador={4} />
+      <FiltroCards legenda="urgentes" contador={2} />
+      <FiltroCards legenda="importantes" contador={2} />
+      <FiltroCards legenda="normal" contador={3} />
+      <FiltroCards ativo legenda="todas" contador={7} />
     </S.Filtros>
   </S.Aside>
 )
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,5 +1,44 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 4 da EBAC',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Pagar o boleto da luz',
+    descricao: 'Pegar o comprovante no gmail',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'Ir a academia',
+    descricao: 'Fazer treino de perna',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.PENDENTE
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>2 tarefas marcadas como: ”todas” e ”termo”</main>
+  <Container>
+    <p>2 tarefas marcadas como: ”todas” e ”termo”</p>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            prioridade={t.prioridade}
+            status={t.status}
+            titulo={t.titulo}
+          />
+        </li>
+      ))}
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
