a2a638c1e5e22f487270415f0ed575f91421db2b
Giancarlo Pagliarini Alves <giancarlopalvess@outlook.com>
2023-08-04 11:59:44 -0300

    adicionando o reducer de filtros de pesquisa

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,21 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCards from '../../components/FiltroCards'
 
 import * as S from './styles'
+import { RootReducer } from '../../store'
+import { alteraTermo } from '../../store/reducers/filtro'
 
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="buscar" />
-    </div>
-    <S.Filtros>
-      <FiltroCards legenda="pendentes" contador={3} />
-      <FiltroCards legenda="concluídas" contador={4} />
-      <FiltroCards legenda="urgentes" contador={2} />
-      <FiltroCards legenda="importantes" contador={2} />
-      <FiltroCards legenda="normal" contador={3} />
-      <FiltroCards ativo legenda="todas" contador={7} />
-    </S.Filtros>
-  </S.Aside>
-)
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="buscar"
+          value={termo}
+          onChange={(e) => dispatch(alteraTermo(e.target.value))}
+        />
+      </div>
+      <S.Filtros>
+        <FiltroCards legenda="pendentes" contador={3} />
+        <FiltroCards legenda="concluídas" contador={4} />
+        <FiltroCards legenda="urgentes" contador={2} />
+        <FiltroCards legenda="importantes" contador={2} />
+        <FiltroCards legenda="normal" contador={3} />
+        <FiltroCards ativo legenda="todas" contador={7} />
+      </S.Filtros>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -39,10 +39,20 @@ const tarefasSlice = createSlice({
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
       state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
+      console.log(action.payload)
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+
+      if (indexTarefa >= 0) {
+        state.itens[indexTarefa] = action.payload
+      }
     }
   }
 })
 
-export const { remover } = tarefasSlice.actions
+export const { remover, editar } = tarefasSlice.actions
 
 export default tarefasSlice.reducer
